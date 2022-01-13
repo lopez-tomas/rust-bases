ce5de9f24b64bce9479ac0a7167fa6d75e7786b4
Tomas Lopez <lopeztomasenrique98@gmail.com>
2022-01-13 15:18:20 -0300

    feat: using curly brackets to print values with println! macro. todo! macro learned.

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world!\n");
+    
+    //todo!("Use curly brackets to print values.");
+    println!("The first letter of the English alphabet is \'{}\' and the last letter is \'{}\'", 'A', 'Z');
 }
