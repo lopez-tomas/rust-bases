6f43423403e953eb21d9038b0d0bee4bdc4b1aa1
Tomas Lopez <lopeztomasenrique98@gmail.com>
2022-01-13 15:05:42 -0300

    feat: debug folder removed from repository.

--- hello-cargo/target/debug/deps/hello_cargo-51a9c123dfff7617.d
+++
@@ -1,5 +0,0 @@
-/home/frozen/Documents/projects/rust-bases/hello-cargo/target/debug/deps/hello_cargo-51a9c123dfff7617: src/main.rs
-
-/home/frozen/Documents/projects/rust-bases/hello-cargo/target/debug/deps/hello_cargo-51a9c123dfff7617.d: src/main.rs
-
-src/main.rs:
--- hello-cargo/target/debug/hello-cargo.d
+++
@@ -1,1 +0,0 @@
-/home/frozen/Documents/projects/rust-bases/hello-cargo/target/debug/hello-cargo: /home/frozen/Documents/projects/rust-bases/hello-cargo/src/main.rs
